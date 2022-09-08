c4e17a90914aa54aa520e6ffb74980a992340feb
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-09-08 14:14:12 +0000

    completed lab

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,38 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title title
+        Movie.create(title: title)
+    end
+
+    def self.first_movie
+        Movie.first
+        end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.last
+    end
+
+    def self.find_movie_with_id id
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes item
+        Movie.find_by(item)
+    end
+
+    def self.update_all_titles title
+        Movie.update(title: title)
+    end
+
+    def self.delete_by_id id
+        Movie.destroy id
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 
 end
